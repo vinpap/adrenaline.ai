0e0a69367500625f4679e585ba22f03a2ca2ff7f
Vincent Papelard <papelardvincent@gmail.com>
2024-05-27 14:38:33 +0200

    Troubleshooting : env variables not found by Azure

--- app.py
+++ app.py
@@ -8,7 +8,8 @@ from utils import hash_new_password, user_exists, save_user, check_password, ret
 
 
 app = Flask(__name__)
-print(os.environ)
+for v in os.environ:
+    print(v)
 app.secret_key = os.environ["FLASK_SESSION_KEY"]
 
 
